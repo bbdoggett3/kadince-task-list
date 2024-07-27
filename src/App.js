9c7468431319570bad30bf4f429bdfe87bf43aa6
bbdoggett3 <bbdoggett3@gmail.com>
2024-07-26 22:50:27 -0600

    create boilerplate for TaskNav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import TaskNav from './components/Task Nav/TaskNav';
 
 function App() {
   return (
     <div className="App">
       <h1>Kadince To-Do List</h1>
+      <TaskNav/>
     </div>
   );
 }
---
+++ src/components/Task Nav/TaskNav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TaskNav.css";
+
+const TaskNav = () => {
+    return (
+        <section className="task-nav-container">
+            <h2>This is my Task Navigation Component</h2>
+        </section>
+    )
+}
+
+export default TaskNav;
