f8e1d08cf08d576f9a7ab2638028c1ee1e05fe5e
bbdoggett3 <bbdoggett3@gmail.com>
2024-07-27 22:12:49 -0600

    create the nav bar content

--- src/components/Task Nav/TaskNav.jsx
+++ src/components/Task Nav/TaskNav.jsx
@@ -5,6 +5,15 @@ const TaskNav = () => {
     return (
         <section className="task-nav-container">
             <h2>Task Navigation Component</h2>
+            <nav>
+                <ul className="task-nav">
+                    <li className="special-li"><a className="new-task" href="#new-task">+ New Task</a></li>
+                    <li><a href="#today">Today</a></li>
+                    <li><a href="#pending">Pending</a></li>
+                    <li><a href="#completed">Completed</a></li>
+                    <li><a href="#all-tasks">All Tasks</a></li>
+                </ul>
+            </nav>
         </section>
     )
 }
